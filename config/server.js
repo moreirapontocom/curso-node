aa335492e1bf0d6fc4df5f0eaa65ab6dbaa7abc2
Lucas Moreira <moreirapontocom@gmail.com>
2019-09-05 15:26:17 -0300

    Static resources

--- config/server.js
+++ config/server.js
@@ -7,6 +7,8 @@ var expressValidator = require('express-validator');
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+app.use(express.static('./app/public'));
+
 // Should be placed here (before auto-loadings) because they're middlewares
 
 app.use(bodyParser.urlencoded({ extended: true }));
