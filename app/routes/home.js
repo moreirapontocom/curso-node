9846b3d7d6cd2c5fa21e778e7d25f6b9b6fdf6bc
Lucas Moreira <moreirapontocom@gmail.com>
2019-09-04 22:18:16 -0300

    MySQL connection

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,7 +1,20 @@
 module.exports = function(app) {
 
     app.get('/', function(req,res) {
-        res.render('category/home');
+
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'node_mysql',
+            user: 'root',
+            password: 'root',
+            database: 'curso_node'
+        });
+
+        connection.query('SELECT * FROM news', function(err, result) {
+            // res.render('category/home');
+            res.send(result);
+        });
     });
 
 }
