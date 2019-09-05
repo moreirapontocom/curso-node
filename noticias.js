844f8ca59d091291c147b7b00be3c7a5d3d44411
Lucas Moreira <moreirapontocom@gmail.com>
2019-09-04 21:44:48 -0300

    Server responding to requested URL

--- noticias.js
+++ noticias.js
@@ -1,5 +1,15 @@
 var http = require('http');
 
 http.createServer(function(req,res) {
-    res.end('Hello World');
+
+    if (req.url=='/noticias') {
+        res.end('Noticias');
+    } else if (req.url=='/tecnologia') {
+        res.end('Noticias de Tecnologia');
+    } else if (req.url=='/moda') {
+        res.end('Noticias de Moda');
+    } else {
+        res.end('Noticias Gerais');
+    }
+   
 }).listen(4200);
